1fe1e76e36ba8872c2fa121730983f8f93f694e4
abdullah201240 <abdullahalsakib7075@gmail.com>
2024-07-14 06:05:58 +0600

    setup docker

--- server/src/config/database.ts
+++ server/src/config/database.ts
@@ -1,7 +1,7 @@
 import { Sequelize } from 'sequelize';
 
 const db = new Sequelize('MealManagement', 'postgres', '12sakib45', {
-  host: 'localhost',
+  host: 'db',
   dialect: 'postgres',
   logging: false,
 });
